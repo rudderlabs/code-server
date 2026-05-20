33514eb7890f860603c0f376e980c0ac225a7656
Yuvraj Angi <yatinangi@gmail.com>
2026-05-21 01:20:28 +0530

    Revert "test(PRO-5561): skip 2 pre-existing e2e failures unmasked by menubar fix"
    This reverts commit 51f7d6b1d6f41c2742bd472132967c4be74b39ed.

--- test/e2e/openHelpAbout.test.ts
+++ test/e2e/openHelpAbout.test.ts
@@ -2,12 +2,7 @@ import { version } from "../../src/node/constants"
 import { describe, test, expect } from "./baseFixture"
 
 describe("Open Help > About", ["--disable-workspace-trust"], {}, () => {
-  // Pre-existing failure independent of PR #212: this test was already failing
-  // on PR #210 / #211 (merged) but was hidden behind earlier maxFailures stops.
-  // The "Save As" and "Integrated Terminal" navigateMenus failures soaking up
-  // those slots are now fixed; the underlying About-dialog timeout that this
-  // test hits is older and unrelated to PRO-5561.
-  test.skip("should see code-server version in about dialog", async ({ codeServerPage }) => {
+  test("should see code-server version in about dialog", async ({ codeServerPage }) => {
     // Open using the menu.
     await codeServerPage.navigateMenus(["Help", "About"])
 
